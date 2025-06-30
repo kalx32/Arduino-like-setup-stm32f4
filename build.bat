a74a44b28ad5e2abb848ed66ed30de18c46d4767
Abdulrahman <109581838+kalx32@users.noreply.github.com>
2025-06-30 20:46:04 +0300

    Update build.bat
    modified batch script to support build on different computers

--- build.bat
+++ build.bat
@@ -2,6 +2,35 @@
 
 echo off
 
+echo looking for cmake.exe
+
+where cmake.exe >null 2>&1
+if %errorlevel% neq 0 (
+    echo please install cmake and make sure it is added in the path
+) else (
+    echo cmake found
+)
+
+echo looking for ninja.exe
+
+where ninja.exe >null 2>&1
+if %errorlevel% neq 0 (
+    echo please install ninja and make sure it is added in the path
+) else (
+    echo ninja found
+)
+
+echo looking for gnu-arm embedded toolchain
+
+where arm-none-eabi-gcc.exe >null 2>&1
+if %errorlevel% neq 0 (
+    echo please install GNU-ARM embedded toolchain for Windows i.e arm-none-eabi-gcc, g++, etc
+)
+
+echo running cmake
+
+cmake . --preset 
+
 ninja -C ./STM32/build
 if %errorlevel% neq 0 (
     echo build finish with error
